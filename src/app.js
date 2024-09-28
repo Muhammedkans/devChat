918e6d0cc2ee4e6288578d8ab43434df12a0663b
MuhammedKans <kanskabeer@gmail.com>
2024-09-28 14:47:09 +0530

    middlewares

--- src/app.js
+++ src/app.js
@@ -3,15 +3,33 @@ const express = require('express');
 
 const app = express();
 
-app.use("/pro",(req,res)=>{
+
+
+app.use("/profile",  (req,res,next)=>{
+  console.log("haii")
+  const token= 'xywz'
+  if(token=== 'xyz'){
+    res.send('hahha')
+  }
+  else{
+    next();
+  }
+ 
+  
+ },(req,res,next)=>{
+ next()
+},(req,res)=>{
+  res.status(404).send("something went wrong")
+})
+
+
+app.get("/profile/name",(req,res)=>{
   
   res.send("hahah")
   console.log(" hloii starting");
 });
 
-app.use("/",  (req,res)=>{
- res.send(" helloworld ");
-});
+
 
 app.listen(7777,()=>{
   console.log("starting server running on port 3000");
